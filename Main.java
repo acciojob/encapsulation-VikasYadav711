c915cf6a847872a674d797af99659553a7f7e9cf
Vikas Yadav <128998971+VikasYadav711@users.noreply.github.com>
2024-12-18 18:58:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package Spring.package1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        RWOnly obj= new RWOnly();
+        obj.setter(10);
+        int res= obj.getter();
+        System.out.println(res);
+    }
+    
+
+    /*
+    Error encountered while acessing the private variable of RWonly class :
+    Spring\package1\Main.java:8: error: num has private access in RWOnly
+        obj.num=10;
+                ^
+        Spring\package1\Main.java:9: error: cannot find symbol
+                System.out.println(num);
+                                ^
+        symbol:   variable num
+        location: class Main
+        2 errors
+
+        Conclusion: We can't acess the private variable of another class of same package directly through the obj.
+     * 
+     * 
+     */
+}
---
+++ RWOnly.java
@@ -0,0 +1,14 @@
+package Spring.package1;
+
+public class RWOnly {
+    
+    private int num;
+
+    public void setter(int val){
+        this.num= val;
+    }
+
+    public int getter(){
+        return num;
+    }
+}
