3bebe61c1191b5ed5bcbf7dd312276e19ee60516
VIKAS YADAV <vikas.savy117@gmail.com>
2025-01-29 00:17:21 +0530

    updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        
+        RWOnly obj2 =new RWOnly();
+        obj2.setter(10);
+        int res= obj2.getter();
+        System.out.println(res);
+    }
+    
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,13 @@ package com.driver;
 
 public class RWOnly {
     
+    private int sum;
+
+    public void setter(int val){
+        this.sum=val;
+    }
+
+    public int getter(){
+        return sum;
+    }
 }
